6b073278a2ed39917341bea29f225a8539aa5865
Debbieliang9 <debbieliang-123@berkeley.edu>
2021-05-06 10:18:01 -0700

    launch file scratches

---
+++ launch/orb_slam2_terC.bash
@@ -0,0 +1,2 @@
+sudo docker pull debbieliang/ros_open:posestamped 
+sudo docker run --network host --rm -it debbieliang/ros_open:posestamped /bin/bash -c "./orb_slam_2_ros/build/devel/lib/orb_slam2_ros/orb_slam2_ros_pss"
---
+++ launch/orb_slam2_terD.bash
@@ -0,0 +1,2 @@
+sudo docker pull debbieliang/ros_open:posestamped 
+sudo docker run --network host --rm -it debbieliang/ros_open:posestamped /bin/bash -c "cd /RosImageFolderPublisher && source devel/setup.bash && cd src && rosrun image_folder_publisher image_folder_publisher.py _image_folder:=./../../rgbd_dataset_freiburg1_xyz/rgb _topic_name=/camera/image_raw"
