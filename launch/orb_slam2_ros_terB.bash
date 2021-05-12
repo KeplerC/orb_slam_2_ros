cf77647822850dc8d0cdc699df19c27fc38bcf64
Debbieliang9 <debbieliang-123@berkeley.edu>
2021-05-12 14:13:08 -0700

    deleted wrong name

--- launch/orb_slam2_ros_terB.bash
+++
@@ -1,2 +0,0 @@
-sudo docker pull debbieliang/ros_open:posestamped 
-sudo docker run --network host --rm -it debbieliang/ros_open:posestamped /bin/bash -c "cd /orb_slam_2_ros/build && source devel/setup.bash && cd .. && roslaunch orb_slam2_ros orb_slam2_r200_mono.launch"
