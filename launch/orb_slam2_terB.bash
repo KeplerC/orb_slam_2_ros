32ab0343e1b2a725f876f3ea2f918907b20ca05f
Debbieliang9 <debbieliang-123@berkeley.edu>
2021-06-17 20:19:33 -0700

    modified terB & added log example

--- launch/orb_slam2_terB.bash
+++ launch/orb_slam2_terB.bash
@@ -1,4 +1,3 @@
 sudo apt install -y docker.io
 sudo docker pull debbieliang/ros_open:posestamped
-sudo docker run --network host --rm -it debbieliang/ros_open:posestamped roscore
 sudo docker run --network host --rm -it debbieliang/ros_open:posestamped /bin/bash -c "cd /orb_slam_2_ros/build && source devel/setup.bash && cd .. && roslaunch orb_slam2_ros orb_slam2_r200_mono.launch"
